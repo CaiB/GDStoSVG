6f8f49bfa77b5f3073aa42a5107775db3cf50762
CaiB <cai.biesinger@outlook.com>
2020-12-19 00:27:54 -0800

    Added references, enabled nullable reference types, and added basic layer config reader.

---
+++ LayerConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace GDStoSVG
+{
+    public class LayerConfig
+    {
+        /// <summary> The currently configured </summary>
+        public static Layer[]? Layers { get; private set; }
+
+        /// <summary> Populates <see cref="Layers"/> with data from a CSV file. </summary>
+        /// <remarks> CSV file should be formatted with no header, with 4 entries per line: [Name:string], [ID:byte], [Colour:uint hex], [Opacity:float] </remarks>
+        /// <param name="configFile"> The filename of a CSV file to read from. </param>
+        public void ReadConfig(string configFile)
+        {
+            List<Layer> LayerList = new List<Layer>();
+            using (StreamReader Reader = new StreamReader(configFile))
+            {
+                string? Line = Reader.ReadLine();
+                Regex CSVParse = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                while (Line != null)
+                {
+                    string[] Parts = CSVParse.Split(Line);
+                    Layer Layer = new Layer
+                    {
+                        Name = Parts[0],
+                        ID = byte.Parse(Parts[1]),
+                        Colour = Convert.ToUInt32(Parts[2], 16), // hex
+                        Opacity = float.Parse(Parts[3])
+                    };
+                    LayerList.Add(Layer);
+                    Line = Reader.ReadLine();
+                }
+            }
+            Layers = LayerList.ToArray();
+        }
+    }
+
+    /// <summary> Stores info about a layer in the GDS file, corresponding to a certain material/process in the chip. E.g. polysilicon for gates. </summary>
+    public class Layer
+    {
+        /// <summary> Name used to label output polygons. Any name valid in SVG is fine, does not matter to GDSII import. </summary>
+        public string Name { get; set; } = "Unnamed Layer";
+
+        /// <summary> The layer ID used in the GDSII file. </summary>
+        public byte ID { get; set; }
+
+        /// <summary> The colour to give the layer in the output SVG. </summary>
+        public uint Colour { get; set; }
+
+        /// <summary> The opacity to assign the objects in the SVG file. </summary>
+        public float Opacity { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace GDStoSVG
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Expected parameters
+            // CSV file, GDS gile, SVG file, layer detect mode switch
         }
     }
 }
